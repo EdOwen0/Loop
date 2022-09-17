9d8218e8aab500081e0da48b0cbf94a1db65eac0
EdOwen0 <edward.owen2134@gmail.com>
2022-09-17 19:40:58 +0100

    up

--- LoopEdit/LoopEdit/LoopEdit.cpp
+++ LoopEdit/LoopEdit/LoopEdit.cpp
@@ -9,6 +9,40 @@
 
 bool bPrintProcesses;
 
+
+HANDLE GetHandle(DWORD pid) 
+{
+    return OpenProcess(WRITE_DAC | READ_CONTROL | PROCESS_VM_READ, false, pid);
+}
+
+
+HMODULE GetModule(DWORD pid)
+{
+    HMODULE hMods[1024];
+    HANDLE pHandle = GetHandle(pid);
+    DWORD cbNeeded;
+    unsigned int i;
+
+    if (EnumProcessModules(pHandle, hMods, sizeof(hMods), &cbNeeded))
+    {
+        for (i = 0; i < (cbNeeded / sizeof(HMODULE)); i++)
+        {
+            TCHAR szModName[MAX_PATH];
+            if (GetModuleFileNameEx(pHandle, hMods[i], szModName, sizeof(szModName) / sizeof(TCHAR)))
+            {
+                std::wstring wstrModName = szModName;
+                std::wstring wstrModContain = L"TestLoop.exe";
+                if (wstrModName.find(wstrModContain) != std::string::npos)
+                {
+                    CloseHandle(pHandle);
+                    return hMods[i];
+                }
+            }
+        }
+    }
+    return nullptr;
+}
+
 DWORD GetPID() 
 {
   PROCESSENTRY32 entry;
@@ -32,27 +66,19 @@ DWORD GetPID()
   return 0x00000000;
 }
 
-
-
-
 int main()
 {
   bPrintProcesses = false;
   DWORD pid = GetPID();
-  HANDLE hTargetProcessToken = OpenProcess(WRITE_DAC | READ_CONTROL | PROCESS_VM_READ, false, pid);
-  std::string mm;
-  LPSTR pBuffer[128];
-  pBuffer = mm;
+  //wchar_t pBuffer[128];
+  //wchar_t bufferLocation = &pBuffer;
 
-  DWORD FileName = GetProcessImageFileNameA(hTargetProcessToken, *pBuffer, 0x00FF);
+  HMODULE Module = GetModule(pid);
+  DWORD BaseAddress = (DWORD)Module;
+  std::cout << BaseAddress << "\n";
 
+  // DWORD FileName = GetProcessImageFileNameA(hTargetProcessToken, bufferLocation, 0x00FF);
 
-  std::cout << &hTargetProcessToken << std::endl;
-  std::cout << hTargetProcessToken << std::endl;
-  hTargetProcessToken;
-  
-  
-  CloseHandle(hTargetProcessToken);
   std::cin.ignore();
   return 0;
 }
